2613fd174f8858d60c247b34d23664c2c41c76f5
Timothy G. Flynn <tgflynn@cogniception.com>
2026-04-29 19:24:20 -0400

    Playing with kregx.lisp

--- src/kregx.lisp
+++ src/kregx.lisp
@@ -1,7 +1,18 @@
 
 (in-package :IC)
 
+(defparameter *JULIUS-REGX-TEST-TARGET-1* "test")
+(defparameter *JULIUS-REGX-TEST-LIST-1* '( "test" ))
+(defparameter *JULIUS-REGX-TEST-LIST-2* (list #\t #\* #\. ) )
+;(defparameter *JULIUS-REGX-TEST-LIST-2A* (list |.| ) )
+;(defparameter *JULIUS-REGX-TEST-LIST-2B* (list |*| ) ) ; read as (nil)
+;(defparameter *JULIUS-REGX-TEST-LIST-2C* (list |alkh| ) ) ; variable alkh is unbound
+;(defparameter *JULIUS-REGX-TEST-LIST-3* (list #\t #\* #\. |*| |.| ) )
+;(defparameter *JULIUS-REGX-TEST-LIST-4* (list #\t #\* #\. |.| ) )
+(defparameter *JULIUS-REGX-TEST-CHAR-1* #\A)
+
+
 (defparameter *JULIUS-REGX-CHARS* '(
-                                    ;( #\* :JULIUS-REGX-WILD-SINGLE )
-                                    ;( #\. :JULIUS-REGX-WILD-MULT )
-                                    )
+                                    ( #\. :JULIUS-REGX-WILD-SINGLE )
+                                    ( #\* :JULIUS-REGX-WILD-MULT )
+                                    ))
