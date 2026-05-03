f0539803bc80209074e3b512fb31251435c85e9b
Timothy G. Flynn <tgflynn@cogniception.com>
2026-05-03 02:25:29 -0400

    Updated startup.lisp

--- startup.lisp
+++ startup.lisp
@@ -4,19 +4,7 @@
 
 (format t "*PROJ-ASDF-FILE* = ~a~%" *PROJ-ASDF-FILE*)
 
-;; (let* ((home (user-homedir-pathname))
-;;        (hlen (length (directory-namestring home)))
-;;        (project-asdf (make-pathname :directory
-;;                                     `(:absolute
-;;                                       ,(subseq (directory-namestring home) 1 (- hlen 1))
-;;                                       "projects" "lisp" "projects" "julius")
-;;                                     :name "julius"
-;;                                     :type "asd"))
-;;        )
-;;   (setf *PROJ-ASDF* project-asdf))
-
 (asdf:initialize-source-registry *PROJ-ASDF-FILE*)
 
 (asdf:load-system :julius)
 
-;(require :julius)
