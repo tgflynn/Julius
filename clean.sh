afbb3f6c53d86b0b70722af5309a183a0318bb3b
Timothy G. Flynn <tgflynn@cogniception.com>
2026-05-03 02:45:51 -0400

    Fixed cleanup script

--- clean.sh
+++ clean.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 PROJ_DIR="${HOME}/projects/lisp/projects/julius"
-CACHE_DIR="${HOME}/.cache/common-lisp/sbcl-2.6.3-linux-x64/home/flynn/projects/lisp/projects/julius"
+CACHE_DIR="${HOME}/.cache/common-lisp/sbcl-2.6.3-linux-x64/${PROJ_DIR}"
 
 find "${CACHE_DIR}" -iname '*.fasl' -exec rm '{}' ';'
 find "${PROJ_DIR}" -iname '*.fasl' -exec rm '{}' ';'
